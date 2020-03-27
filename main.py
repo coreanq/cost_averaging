25e7b988d2e023ab0b7f394f01371eb82e0fef10
Charles <coreanq@gmail.com>
2020-03-28 01:29:39 +0900

    rebalancing 주문 구현

--- main.py
+++ main.py
@@ -22,45 +22,23 @@ server_url = "https://api.upbit.com"
 
 class UpbitRebalancing(QObject):
     sigInitOk = pyqtSignal()
-    sigConnected = pyqtSignal()
-    sigDisconnected = pyqtSignal()
-    sigTryConnect = pyqtSignal()
-    sigGetConditionCplt = pyqtSignal()
-    sigSelectCondition = pyqtSignal()
-    sigWaitingTrade = pyqtSignal()
-    sigRefreshCondition = pyqtSignal()
-
-    sigStateStop = pyqtSignal()
-    sigStockComplete = pyqtSignal()
-
-    sigConditionOccur = pyqtSignal()
-    sigRequestInfo = pyqtSignal()
-    sigRequestEtcInfo = pyqtSignal()
-
-    sigGetBasicInfo = pyqtSignal()
-    sigGetEtcInfo = pyqtSignal()
-    sigGet5minInfo = pyqtSignal()
-    sigGetHogaInfo = pyqtSignal()
-    sigTrWaitComplete = pyqtSignal()
-
-    sigBuy = pyqtSignal()
-    sigNoBuy = pyqtSignal()
-    sigRequestRealHogaComplete = pyqtSignal()
     sigError = pyqtSignal()
-    sigRequestJangoComplete = pyqtSignal()
-    sigCalculateStoplossComplete = pyqtSignal()
-    sigStartProcessBuy = pyqtSignal()
-    sigStopProcessBuy = pyqtSignal()
-    sigTerminating = pyqtSignal()
+    sigStateStop = pyqtSignal()
 
     def __init__(self):
         super().__init__()
         self.fsm = QStateMachine()
         self.timerRequestOrderbook = QTimer()
+        self.timerRequestAccountInfo = QTimer()
         self.currentTime = datetime.datetime.now()
-        self.current_price = 0 # 현재 가격의 경우 매도호가 2로 정함 (1부터 시작 )
+
+        self.current_ask_price = 0         
+        self.current_bid_price = 0 # 현재 가격의 경우 매수호가 2로 정함 (1부터 시작 )
+
         self.crypto_market_name = 'KRW-XRP'
         self.account_info = []
+        self.fiat_balance = 0
+        self.crypto_balance = 0
 
 
         self.init()
@@ -73,28 +51,35 @@ class UpbitRebalancing(QObject):
         20200327
         EXCHANGE API
         [주문]
-
         초당 8회, 분당 200회
-        [주문 외 API]
 
+        [주문 외 API]
         초당 30회, 분당 900회
+
         [Exchange API 추가 안내 사항]
 
         QUOTATION API
         1) Websocket 연결 요청 수 제한
-
         초당 5회, 분당 100회
-        2) REST API 요청 수 제한
 
+        2) REST API 요청 수 제한
         분당 600회, 초당 10회 (종목, 캔들, 체결, 티커, 호가별)
         '''
     def init(self):
-        self.timerRequestOrderbook.setInterval(1000)
+        self.timerRequestOrderbook.setInterval(200)
         self.timerRequestOrderbook.timeout.connect(self.onTimerRequestOrderbookTimeout) 
+
+        self.timerRequestAccountInfo.setInterval(1000)
+        self.timerRequestAccountInfo.timeout.connect(self.onTimerRequestAccountInfoTimeout) 
 
     @pyqtSlot()
     def onTimerRequestOrderbookTimeout(self):
         self.getOrderbook(self.crypto_market_name)
+        pass
+
+    @pyqtSlot()
+    def onTimerRequestAccountInfoTimeout(self):
+        self.getAccountInfo()
         pass
 
 
@@ -140,10 +125,85 @@ class UpbitRebalancing(QObject):
         account_info = self.account_info
 
         for item in account_info:
-            if( item['currency'] == 'KRW'):
-                print( round( float(item['balance']), 2 ))
-            if( item['currency'] == 'XRP' ):
-                print( round( float(item['balance']) * self.current_price, 2 )
+            currency_key = 'currency'
+            balance_key = 'balance'
+            if( item[currency_key] == 'KRW'):
+                self.fiat_balance = round( float(item[balance_key]), 2 )
+            if( item[currency_key] == 'XRP' ):
+                #낮은게 좋으므로 매수 호가 기준으로 삼음
+                self.crypto_balance = round( float(item[balance_key]) * self.current_price, 2 ) 
+
+        if( self.fiat_balance == 0 or self.crypto_balance == 0 ):
+            return 
+
+
+        balance_sum = self.fiat_balance + self.crypto_balance
+        fiat_percent = round(self.fiat_balance/balance_sum * 100, 2)
+        crypto_percent = round(self.crypto_balance/balance_sum * 100, 2) 
+        print( 'fiat: {} %, crypto {} %'.format(
+            fiat_percent
+            ,crypto_percent
+        ))
+
+        if( abs(fiat_percent - crypto_percent) > 2 ):
+            if( fiat_percent > crypto_percent ):
+                # 현금 비중이 높은 경우 
+                order_balance = round((self.fiat_balance - self.crypto_balance) / 2) 
+                #buy
+                self.rebalancing('bid', order_balance)
+            else:
+                # 암호화폐 비중이 높은 경우
+                #sell
+                order_balance = round((self.crypto_balance - self.fiat_balance) /2 )
+                self.rebalancing('ask', order_balance )
+
+    def rebalancing(self, side, order_balance):
+        print(util.whoami() )
+        query = ''
+        volume = 2.5
+        if( side == 'bid' ):
+            # 암호화폐 매수 매도호가 기준 
+            volume = round(order_balance / self.current_ask_price, 2)
+            query = {
+                'market': 'KRW-XRP',
+                'side': 'bid',
+                'volume': volume,
+                'price': str(self.current_ask_price),
+                'ord_type': 'limit',
+            }
+        else:
+            # 암호화폐 매도므로 매수호가 기준
+            volume = round(order_balance / self.current_bid_price, 2)
+            query = {
+                'market': 'KRW-XRP',
+                'side': 'ask',
+                'volume': volume,
+                'price': str(self.current_bid_price),
+                'ord_type': 'limit',
+            }
+
+        query_string = urlencode(query).encode()
+
+        m = hashlib.sha512()
+        m.update(query_string)
+        query_hash = m.hexdigest()
+
+        payload = {
+            'access_key': access_key,
+            'nonce': str(uuid.uuid4()),
+            'query_hash': query_hash,
+            'query_hash_alg': 'SHA512',
+        }
+
+        jwt_token = jwt.encode(payload, secret_key).decode('utf-8')
+        authorize_token = 'Bearer {}'.format(jwt_token)
+        headers = {"Authorization": authorize_token}
+
+        response = requests.post(server_url + "/v1/orders", params=query, headers=headers)
+        print(json.dumps( response.json(), indent=2, sort_keys=True) )
+        pass
+
+
 
     def getOrderbook(self, market_name):
         url =  server_url + "/v1/orderbook"
@@ -152,19 +212,26 @@ class UpbitRebalancing(QObject):
         output_list = response.json()
 
         for item in output_list:
-            if( len(item['orderbook_units']) == 15 ):
-                # 1 매수호가
-                bid_price = item[0]['bid_price']
+            orderbook_key = 'orderbook_units'
+            bid_price_key = 'bid_price'
+            ask_price_key = 'ask_price'
+            if( len(item[orderbook_key]) == 15 ):
+                # 1 매수호가 낮은 가격이 좋으므로 매수 1호가 기준으로 현재 가격 결정함
+                self.current_price = item[orderbook_key][0][bid_price_key]
                 # 2 매수호가
-                bid_price = item[1]['bid_price']
+                bid_price = item[orderbook_key][1][bid_price_key]
                 # 현재가는 좀 낮아 보이는게 나으므로 매수호가로 처리함
-                self.current_price = bid_price 
+                self.current_bid_price = bid_price 
+
+                # 1 매도호가
+                ask_price = item[orderbook_key][0][ask_price_key]
                 # 2 매도호가
-                # ask_price = item[1]['ask_price']
+                ask_price = item[orderbook_key][1][ask_price_key]
+                self.current_ask_price = ask_price
+                self.sigInitOk.emit()
+            else:
+                self.sigError.emit()
 
-                self.getAccountInfo()
-
-                print('ok')
             pass
         # print(json.dumps( response.json(), indent=2, sort_keys=True) )
 
@@ -186,6 +253,7 @@ class UpbitRebalancing(QObject):
 
 
         initState.addTransition(self.sigInitOk, standbyState)
+        standbyState.addTransition(self.sigError, initState)
 
         # #state entered slot connect
         mainState.entered.connect(self.mainStateEntered)
@@ -205,12 +273,14 @@ class UpbitRebalancing(QObject):
 
     @pyqtSlot()
     def initStateEntered(self):
-        # print(util.whoami())
+        print(util.whoami())
         self.timerRequestOrderbook.start()
+        self.timerRequestAccountInfo.stop()
 
     @pyqtSlot()
     def standbyStateEntered(self):
         print(util.whoami())
+        self.timerRequestAccountInfo.start()
 
     @pyqtSlot()
     def finalStateEntered(self):
@@ -225,14 +295,6 @@ if __name__ == "__main__":
     form = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(form)
-
-    # ui.btnMakeExcel.clicked.connect(objKiwoom.onBtnMakeExcelClicked )
-    # ui.btnStart.clicked.connect(objKiwoom.onBtnStartClicked)
-
-    # ui.btnYupjong.clicked.connect(objKiwoom.onBtnYupjongClicked)
-    # ui.btnJango.clicked.connect(objKiwoom.onBtnJangoClicked)
-    # ui.btnChegyeol.clicked.connect(objKiwoom.onBtnChegyeolClicked)
-    # ui.btnCondition.clicked.connect(objKiwoom.onBtnConditionClicked)
 
     # ui.lineCmd.textChanged.connect(objKiwoom.onLineCmdTextChanged)
     # ui.btnRun.clicked.connect(objKiwoom.onBtnRunClicked)
