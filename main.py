21e3239f5b1da2f6b435837f34bf54c4ba1c6c4e
Charles Park <coreanq@gmail.com>
2023-07-05 16:33:31 +0900

    현금 있는 경우 무조건 사도록 변경, simulate 결과 update(적립식)

--- main.py
+++ main.py
@@ -126,60 +126,87 @@ class UpbitRebalancing(QObject):
             if( isLocked == True ):
                 return
 
-            result = self.upbitIf.checkAssetInfo(fiat_balance, self.current_price, crypto_balance)
+            # self.processShannonsDemonRebalance(fiat_balance, crypto_balance)
 
-            if( result == None ):
-                self.sigError.emit()
-
-            order_type = result['order_type']
-            order_balance = result['order_balance']
-
-            balance_sum = fiat_balance + crypto_balance * self.current_price
-            fiat_percent = round(fiat_balance/balance_sum * 100, 2)
-            crypto_percent = round( (crypto_balance * self.current_price )/balance_sum * 100, 2) 
-
-            self.sigCryptoPercentChanged.emit( str(crypto_percent) + "%" )
-            self.sigFiatPercentChanged.emit( str(fiat_percent) + "%" )
-
-            self.sigCryptoBalanceChanged.emit('{:,.2f}({})'.format(crypto_balance, self.current_price) )
-            self.sigFiatBalanceChanged.emit( '{:,.0f}'.format(fiat_balance) )
-
-            self.sigCurrentBalanceChanged.emit(fiat_balance, crypto_balance)
-
-            order_price = 0
-            # 현재 거래 진행중인 거래 확인  
-            self.upbitIf.getOrder()
-            
-
-            # 매수  
-            if( order_type == 'bid' ):
-                order_price = self.current_ask_price
-            # 매도 
-            elif( order_type == 'ask' ):
-                order_price = self.current_bid_price
-            else: # none
-                return
 
             #WARNING: 현금으로 매수 후 잔고 정보 조회시 crypto 잔고가 바로 업데이트 되지 않느 오류가 있으므로 주의 
             # 현재 거래 진행중이면 make order 수행 금지 
             if( self.upbitIf.hasWaitInOrder() == False ):
+                
+                if( fiat_balance > self.current_bid_price and fiat_balance > 5000 ): # 최소 주문 금액 충족 확인 
+                    maemae_type = "매수"
+                    order_type = 'bid'
+                    order_price = self.current_bid_price
+                    order_balance = fiat_balance * 0.9995  # 수수료 고려 제외 시킴 
 
-                maemaeType = ''
-                if( order_type == 'bid' ):
-                    maemaeType = '매수'
-                else:
-                    maemaeType = '매도'
+                    if( self.tradeOn == True ):
+                        printLog = '{} {} {} {}\n'.format( util.cur_time_msec(), maemae_type, order_price, order_balance ) 
+                        print( printLog )
+                        util.save_log( printLog, subject= "{} 요청".format( maemae_type ) )
+
+                        print('잔고: \n{}\n'.format( json.dumps( self.current_account_info, indent=2, sort_keys=True) ) )
+                        self.upbitIf.makeOrder(order_type, order_price, order_balance, False)
+            else:
+                print("\nMake Order avaliable {}".format( self.upbitIf.wait_order_uuids))
+
+        pass
+
+    def processShannonsDemonRebalance(self, fiat_balance, crypto_balance):
+
+        result = self.upbitIf.checkAssetInfo(fiat_balance, self.current_price, crypto_balance)
+
+        if( result == None ):
+            self.sigError.emit()
+
+        order_type = result['order_type']
+        order_balance = result['order_balance']
+
+        balance_sum = fiat_balance + crypto_balance * self.current_price
+        fiat_percent = round(fiat_balance/balance_sum * 100, 2)
+        crypto_percent = round( (crypto_balance * self.current_price )/balance_sum * 100, 2) 
+
+        self.sigCryptoPercentChanged.emit( str(crypto_percent) + "%" )
+        self.sigFiatPercentChanged.emit( str(fiat_percent) + "%" )
+
+        self.sigCryptoBalanceChanged.emit('{:,.2f}({})'.format(crypto_balance, self.current_price) )
+        self.sigFiatBalanceChanged.emit( '{:,.0f}'.format(fiat_balance) )
+
+        self.sigCurrentBalanceChanged.emit(fiat_balance, crypto_balance)
+
+        order_price = 0
+        # 현재 거래 진행중인 거래 확인  
+        self.upbitIf.getOrder()
+        
+
+        # 매수  
+        if( order_type == 'bid' ):
+            order_price = self.current_ask_price
+        # 매도 
+        elif( order_type == 'ask' ):
+            order_price = self.current_bid_price
+        else: # none
+            return
+
+        #WARNING: 현금으로 매수 후 잔고 정보 조회시 crypto 잔고가 바로 업데이트 되지 않느 오류가 있으므로 주의 
+        # 현재 거래 진행중이면 make order 수행 금지 
+        if( self.upbitIf.hasWaitInOrder() == False ):
+
+            maemaeType = ''
+            if( order_type == 'bid' ):
+                maemaeType = '매수'
+            else:
+                maemaeType = '매도'
+
+            if( self.tradeOn == True ):
+                self.upbitIf.makeOrder(order_type, order_price, order_balance, False)
 
                 printLog = '{} {} {} {}\n'.format( util.cur_time_msec(), maemaeType, order_price, order_balance ) 
                 print( printLog )
                 util.save_log( printLog, subject= "{} 요청".format( maemaeType ) )
 
                 print('잔고: \n{}\n'.format( json.dumps( self.current_account_info, indent=2, sort_keys=True) ) )
-                if( self.tradeOn == True ):
-                    self.upbitIf.makeOrder(order_type, order_price, order_balance, False)
-            else:
-                print("\nMake Order pass {}".format( self.upbitIf.wait_order_uuids))
-
+        else:
+            print("\nMake Order pass {}".format( self.upbitIf.wait_order_uuids))
         pass
         
     def createState(self):
